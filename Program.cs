0d31a5368f542cd25e76d0cb1e9f532a7d78e410
Rajeev <rajeev.shm@gmail.com>
2017-10-29 21:19:15 +0000

    added for test

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace hwapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            Console.WriteLine("\nWhat is your name? ");
+            var name = Console.ReadLine();
+            var date = DateTime.Now;
+            Console.WriteLine($"\nHello, {StringLibrary.StartsWithUpper(name)}, on {date:d} at {date:t}!test");
+            Console.Write("\nPress any key to exit...");
+            Console.ReadKey(true);
+        }
+    }
+}
